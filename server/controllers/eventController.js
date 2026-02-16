7c5895e232f681b503682e75e7b8e52d494b528b
ramkore <ramesh@sreyas.ac.in>
2026-02-16 19:57:55 +0530

    Add error details to API responses for debugging

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -39,7 +39,7 @@ const getEvents = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Server Error' });
+        res.status(500).json({ message: 'Server Error', error: error.message });
     }
 };
 
